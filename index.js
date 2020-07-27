7a26b5de823770dd63f4f34c387bda9668199e5b
Scott Buckstar Regina Buckley <Buckstar@imac.lan>
2020-07-27 12:47:58 -0400

    added word function

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 require("@babel/core").transform("code");
 
 let checkWord = require('check-word'),
-    words     = checkWord('en'); // setup the language for check, default is en
+    words = checkWord('en'); // setup the language for check, default is en
 
-let list =  require('./list.js');
+let list = require('./list.js');
 
 list = list.list;
 
-
+// source: https://codereview.stackexchange.com/a/7042
+function getCombinations(chars) {
+    var result = [];
+    var f = function (prefix, chars) {
+        for (var i = 0; i < chars.length; i++) {
+            result.push(prefix + chars[i]);
+            f(prefix + chars[i], chars.slice(i + 1));
+        }
+    }
+    f('', chars);
+    return result;
+}
 
 const getRandomLetters = (number) => {
     let letter = [];
     let finalList = [];
     for (let [first, second] of Object.entries(list)) {
         // Ternary conditional that appends etaoin shrdlu twice and then does random selection of da letters
-        second > 1 ? letter = letter.concat( new Array(second).fill(first)): letter.push(first);
+        second > 1 ? letter = letter.concat(new Array(second).fill(first)) : letter.push(first);
     }
-    for(const x of Array(number).keys()){
+    for (const x of Array(number).keys()) {
         // takes random element of array with duplicates for more common letters, push to the final list
         finalList.push(letter[Math.floor(Math.random() * letter.length)]);
     }
     return finalList;
 }
 
-console.log(getRandomLetters(15));
 
+const getWords = (array) => {
+    let results = getCombinations(array)
+    let wordArray = []
+    console.log(array)
+    // console.log(results)
+    for (result of results) {
+        if (words.check(result)) {
+            wordArray.push(result)
+        }
+    }
+    return wordArray
+}
 
-    
-console.log(words.check('dog')); // true
-words.check('perro'); // false
-words.check('hi'); // true
+console.log(getWords(getRandomLetters(10)));
