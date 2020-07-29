303070b0e65c1fc33db6ea5828ef0337fb1b181b
rachelvfernandes <44947279+rachelvfernandes@users.noreply.github.com>
2020-07-29 13:27:43 -0400

    Delete blob.js

--- public/blob.js
+++
@@ -1,9 +0,0 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/ZjVyKXp9hec
-
-function Player(name, score) {
-  this.name = name;
-  this.score = score;
-}
