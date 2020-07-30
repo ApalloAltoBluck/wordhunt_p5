800aea0b2f4784614f309bf33f06f3894b809594
Rachel Fernandes <rachel@MacBook-Pro.fios-router.home>
2020-07-30 12:12:31 -0400

    changed port

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ function Game(p1, p2) {
 
 var express = require('express'); // Using express: http://expressjs.com/
 var app = express();
-var server = app.listen(process.env.PORT, listen); // process.env.PORT is related to deploying on heroku
+var server = app.listen(process.env.PORT); // process.env.PORT is related to deploying on heroku
 app.use(express.static('public'));
 
 var host = server.address().address;
